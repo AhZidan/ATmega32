e4c0d7f271f485bc606a8f5b6a5f9de2406c53b8
Ahmed <ahmadzidan1@hotmail.com>
2019-02-02 01:42:41 +0200

    Adding Std_Types.h

---
+++ Common/Std_Types.h
@@ -0,0 +1,17 @@
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char uint8;
+typedef unsigned short int    uint16;
+typedef unsigned long  int    uint32;
+
+typedef signed char sint8;
+typedef signed short int sint16;
+typedef signed long  int sint32;
+typedef enum
+{
+    FALSE = 0,
+    TRUE  = 1
+} boolean;
+
+#endif
