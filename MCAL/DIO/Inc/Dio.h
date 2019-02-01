520aae824f04f9b9d9b414a8bcb5bc0202f577f4
Ahmed <ahmadzidan1@hotmail.com>
2019-02-02 00:39:13 +0200

    Added Features: 1- Dio_vidWritePin() 2- Dio_vidSetPortDirection() 3- Dio Exported Types

--- MCAL/DIO/Inc/Dio.h
+++ MCAL/DIO/Inc/Dio.h
@@ -3,4 +3,27 @@
 #include "Std_Types.h"
 
 
+typedef enum
+{
+    Dio_Low = 0,
+    Dio_High
+} enuDioState;
+
+typedef enum
+{
+    Dio_Input = 0,
+    Dio_Output
+} enuDioDirection;
+
+typedef enum
+{
+    Dio_PORTA, 
+    Dio_PORTB,
+    Dio_PORTC,
+    Dio_PORTD
+} enuDioPort;
+
+void Dio_vidWritePin(enuDioPort Port, uint8 Pin, enuDioState State);
+void Dio_vidSetPortDirection(enuDioPort Port, enuDioDirection Direction);
+void Dio_vidSetPinDirection(enuDioPort Port, enuDioDirection Direction, uint8 Pin);
 #endif
--- MCAL/DIO/Src/Dio.c
+++ MCAL/DIO/Src/Dio.c
@@ -53,3 +53,115 @@ void Dio_vidSetPinDirection(enuDioPort Port, enuDioDirection Direction, uint8 Pi
 			break;
 	}
 }
+
+/*****************************************************************************************
+! Description: Set the direction of a DIO port                                           *
+! Name       : Dio_vidSetPortDirection                                                   *
+! Input      : Port ID, Direction (IN, OUT)                                              *
+! Output     : None                                                                      *
+! Last_Author: Ahmed.Yasser                                                              *
+/*****************************************************************************************/
+void Dio_vidSetPortDirection(enuDioPort Port, enuDioDirection Direction)
+{
+	switch(Port)
+	{
+		case Dio_PORTA:
+			if(Direction == Dio_Input)
+			{
+				DDRA = 0xFFU;
+			}
+			else if(Direction == Dio_Output)
+			{
+				DDRA = 0x00U;
+			}
+			break;
+		case Dio_PORTB:
+			if(Direction == Dio_Input)
+			{
+				DDRB = 0xFF;
+			}
+			else if(Direction == Dio_Output)
+			{
+				DDRB = 0x00U;
+			}
+			break;
+		case Dio_PORTC:
+			if(Direction == Dio_Input)
+			{
+				DDRC = 0xFFU;
+			}
+			else if(Direction == Dio_Output)
+			{
+				DDRC = 0x00U;
+			}
+			break;
+		case Dio_PORTD:
+			if(Direction == Dio_Input)
+			{
+				DDRD = 0xFFU;
+			}
+			else if(Direction == Dio_Output)
+			{
+				DDRD = 0x00U;
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+/*****************************************************************************************
+! Description: Write a specific logic state to DIO pin                                   *
+! Name       : Dio_vidWritePin                                                           *
+! Input      : Port ID, Pin Number, Pin Logic State                                      *
+! Output     : None                                                                      *
+! Last_Author: Ahmed.Yasser                                                              *
+/*****************************************************************************************/
+void Dio_vidWritePin(enuDioPort Port, uint8 Pin, enuDioState State)
+{
+	switch(Port)
+	{
+		case Dio_PORTA:
+			if(State == Dio_High)
+			{
+				Set_Bit(PORTA, Pin);
+			}
+			else if(State == Dio_Low)
+			{
+				Clear_Bit(PORTA, Pin);
+			}
+			break;
+		case Dio_PORTB:
+			if(State == Dio_High)
+			{
+				Set_Bit(PORTB, Pin);
+			}
+			else if(State == Dio_Low)
+			{
+				Clear_Bit(PORTB, Pin);
+			}
+			break;
+		case Dio_PORTC:
+			if(State == Dio_High)
+			{
+				Set_Bit(PORTC, Pin);
+			}
+			else if(State == Dio_Low)
+			{
+				Clear_Bit(PORTC, Pin);
+			}
+			break;
+		case Dio_PORTD:
+			if(State == Dio_High)
+			{
+				Set_Bit(PORTD, Pin);
+			}
+			else if(State == Dio_Low)
+			{
+				Clear_Bit(PORTD, Pin);
+			}
+			break;
+		default:
+			break;
+	}
+}
